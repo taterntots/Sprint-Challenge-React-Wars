263e037d5f7ac5f5e340f92b5901a7f7e53627f4
taterntots <matthew.a.weidner@gmail.com>
2019-11-08 13:02:05 -0500

    overrode some stylings in reactstrap using styled components, and answered a few of the study questions.

---
+++ starwars/Study Question Answers.md
@@ -0,0 +1,18 @@
+# What is React JS and what problems does it try and solve? Support your answer with concepts introduced in class and from your personal research on the web.
+
+
+
+# What does it mean to _think_ in react?
+
+
+
+# Describe state.
+
+
+
+# Describe props.
+
+
+
+# What are side effects, and how do you sync effects in a React component to state or prop changes?
+
--- starwars/src/components/CharacterCards.js
+++ starwars/src/components/CharacterCards.js
@@ -2,28 +2,31 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Col } from 'reactstrap';
 import styled from 'styled-components';
 
-const ProfilePic = styled.img`
-    width: 200px;
-    height: 200px;
-`
+// const ProfilePic = styled.img`
+//     width: 200px;
+//     height: 200px;
+// `
 const MainTitle = styled.h1`
     font-size: 1.4rem;
 `
 const SubTitle = styled.h2`
     font-size: 1.0rem;
 `
+const NewCard = styled(Card)`
+    margin-bottom: 10%;
+`
 
 const CharacterCards = props => {
     return(
-        <Col xs='12' s='6' md='4' lg={'3'}>
-            <Card>
+        <Col xs='12' s='6' md='4' lg='3'>
+            <NewCard >
                 <CardImg top width='100%' src='https://source.unsplash.com/random' alt='Card image cap' />
                 {/* <ProfilePic src="https://source.unsplash.com/random" alt="Card image cap" /> */}
                 <CardBody>
                     <MainTitle>{props.name}</MainTitle>
                     <SubTitle>{props.gender}, born in {props.birth_year}</SubTitle>
                 </CardBody>
-            </Card>
+            </NewCard>
         </Col>
     )
 }
