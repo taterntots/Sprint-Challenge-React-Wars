bb6e060f336bf3996c2e401f3cffd2cefaac0630
taterntots <matthew.a.weidner@gmail.com>
2019-11-08 12:33:25 -0500

    Got the first page of characters styled in cards and displaying name, gender, and birth year. MVP achieved!

--- starwars/src/components/CharacterCards.js
+++ starwars/src/components/CharacterCards.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Col } from 'reactstrap';
+import styled from 'styled-components';
+
+const ProfilePic = styled.img`
+    width: 200px;
+    height: 200px;
+`
+const MainTitle = styled.h1`
+    font-size: 1.4rem;
+`
+const SubTitle = styled.h2`
+    font-size: 1.0rem;
+`
 
 const CharacterCards = props => {
     return(
-        <div>
+        <Col xs='12' s='6' md='4' lg={'3'}>
             <Card>
-                <CardImg top width="100%" src="https://source.unsplash.com/random" alt="Card image cap" />
+                <CardImg top width='100%' src='https://source.unsplash.com/random' alt='Card image cap' />
+                {/* <ProfilePic src="https://source.unsplash.com/random" alt="Card image cap" /> */}
                 <CardBody>
-                    <CardTitle>{props.name}</CardTitle>
-                    <CardSubtitle>{props.gender}, born in {props.birth_year}</CardSubtitle>
-                    {/* <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText> */}
+                    <MainTitle>{props.name}</MainTitle>
+                    <SubTitle>{props.gender}, born in {props.birth_year}</SubTitle>
                 </CardBody>
             </Card>
-        </div>
+        </Col>
     )
 }
 export default CharacterCards;
--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import CharacterCards from './CharacterCards';
+import { Container, Row } from 'reactstrap';
 
 const CharacterList = () => {
     const [characters, setCharacters] = useState([]);
 
     useEffect(() => {
         axios
-            .get('https://swapi.co/api/people/')
+            .get('https://lambda-swapi.herokuapp.com/api/people/')
             .then(response => {
                 setCharacters(response.data.results);
                 console.log(response.data.results);
@@ -18,16 +19,20 @@ const CharacterList = () => {
     }, [])
 
     return(
-        characters.map(char => {
-            return(
-                <CharacterCards 
-                    key={char.id}
-                    name={char.name}
-                    gender={char.gender}
-                    birth_year={char.birth_year}
-                    />
-            )
-        })
+        <Container>
+            <Row>
+                {characters.map(char => {
+                    return(
+                        <CharacterCards 
+                            key={char.id}
+                            name={char.name}
+                            gender={char.gender}
+                            birth_year={char.birth_year}
+                        />
+                    )
+                })}
+            </Row>
+        </Container>
     )
 }
 export default CharacterList;
